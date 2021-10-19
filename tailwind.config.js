8c3cabee18a62ca9dca0a96e3186e5ad6528ec32
MirkoR89 <mirko.rinelli@gmail.com>
2021-10-19 08:28:52 +0200

    chore: set fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,16 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      red: '#ED3D40',
+      yellow: '#F3BF52',
+      indaco: '#0FB8B1',
+      purple: '#799CDE',
+      grey: '#263238',
+    },
+    fontFamily: {
+      body: ['Nunito', 'sans-serif']
+    },
     extend: {},
   },
   variants: {
